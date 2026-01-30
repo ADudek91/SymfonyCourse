71207e62b1e0539b10f85db572b2f4b5dee610ba
andrzej <andrzej.dudek91@gmail.com>
2026-01-30 13:08:56 +0100

    Symfony course Controller Twig installation and handle

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -5,18 +5,16 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MoviesController extends AbstractController
 {
-    #[Route('/movies/{name}', name:'movies', defaults:['name' => null], methods:['GET', 'HEAD'])]
-    public function index($name): JsonResponse
+    #[Route('/movies', name:'movies')]
+    public function index(): Response
     {
-
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
+        return $this->render('index.html.twig', [
+            'title' => ''
         ]);
     }
 }
